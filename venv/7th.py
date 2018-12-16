a35190aa1e510666e3ecfd9c6e7e378e51fd2030
Edgar <18edox@gmail.com>
2018-12-16 20:57:48 +0400

    fix int int 7th.py

--- venv/7th.py
+++ venv/7th.py
@@ -1,15 +1,29 @@
 #! if Statements if/elif/else
-name = 'edgar'
-age = 25
-workplace = 'voicebuy'
+
+'''age = 25
 height = 173
 weight = 76
-eyes = 'green'
 bio = input('Age:') + input('Weight:') + input('Height:')
 print(bio)
 if int(bio) >= 121565:
     print('sas')
 else:
-    print('not sas')
+    print('not sas')  '''
 
 
+#--------------------------
+
+name = input('your name:')
+age = input('age:')
+#workplace = input('workplace:')
+height = input('height:')
+#weight = input('weight:')
+#eyes = input('color of eyes:')
+if int(age) > 20 and int(height) >= 170:
+    print('Congrats!, You passed  mr. :')
+    if name == edgar:
+        print('you are finished mr.')
+    else:
+        print("you lose")
+else:
+    print("you lose all")
