e0316212dbfc34c9bee5433e26488ed432c095bc
Edgar <18edox@gmail.com>
2018-12-18 00:59:49 +0400

    starting loops 2

--- venv/10th-loops.py
+++ venv/10th-loops.py
@@ -1,6 +1,22 @@
-choice = 'y'
+import datetime
+#choice = 'y'
 
-while choice.lower() == "y":
-    print("Привет")
-    choice = input("Для продолжения нажмите Y, а для выхода любую другую клавишу: ")
-print("Работа программы завешена")
+#while choice.lower() == "y":
+#    print("Привет")
+#    choice = input("Для продолжения нажмите Y, а для выхода любую другую клавишу: ")
+#print("Работа программы завешена")
+
+#now = datetime.datetime.now()
+#while now.time().minute == int(29):
+#    print('Sasoooo')
+
+
+# ! Программа по вычислению факториала
+
+number = int(input("Введите число: "))
+i = 1
+factorial = 1
+while i <= number:
+    factorial *= i
+    i += 1
+print("Факториал числа", number, "равен", factorial)
