39f39d516a7aa4fbf0320b8bc87c1fa8e1801501
edox93 <18edox@gmail.com>
2018-12-19 18:35:56 +0400

    starting loops

---
+++ venv/11th-loops2.py
@@ -0,0 +1,37 @@
+#i = 5
+#while i <= 15:
+#    print(i)
+#    i = i + 2
+
+#number = 3
+#i = 1
+#factorial = 1
+#while i < number:
+#    factorial *= i
+#    i += 1
+#print(factorial)
+
+
+'''n = 100
+
+s = 0
+counter = 1
+while counter <= n:
+    s = s + counter
+    counter += 1
+    print(counter)
+print('Finish') '''
+
+
+number = 3
+factorial = 1
+for i in range(3, number + 1):
+    factorial *= i
+print(factorial)
+
+
+for i in [1, 2, 3, 4, 5]:
+ print(i)
+
+for i in 'EdgarRamazyan':
+    print(i+'0203', end='93')
