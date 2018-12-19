d3f6a7f269bea8157a39e0f4c65546dc73b64927
edox93 <18edox@gmail.com>
2018-12-19 18:34:16 +0400

    starting loops

--- venv/10th-loops.py
+++ venv/10th-loops.py
@@ -1,4 +1,4 @@
-import datetime
+#import datetime
 #choice = 'y'
 
 #while choice.lower() == "y":
@@ -12,11 +12,18 @@ import datetime
 
 
 # ! Программа по вычислению факториала
-
-number = int(input("Введите число: "))
+'''number = int(input("Введите число: "))
 i = 1
 factorial = 1
 while i <= number:
     factorial *= i
     i += 1
+print("Факториал числа", number, "равен", factorial) '''
+#-------------------------------------------------------
+
+# ! Программа по вычислению факториала
+number = int(input("Введите число: "))
+factorial = 1
+for i in range(1, number + 1):
+    factorial *= i
 print("Факториал числа", number, "равен", factorial)
