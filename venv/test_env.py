f57c167b718b24ced15d97e150285c80318895b3
edox93 <18edox@gmail.com>
2019-01-16 18:31:09 +0400

    task: find string in string

---
+++ venv/test_env.py
@@ -0,0 +1,14 @@
+word_a = 'Aloslosha'
+word_b = 'lo'
+while  len(word_a) !=0:
+    word_c = word_a.find(word_b)
+
+
+
+
+print(word_b)
+#index = word_a.find(word_b)
+#len(word_b)
+#index2 = word_a.find(word_b,index)
+
+
